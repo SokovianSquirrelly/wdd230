7f35cb192f479d2051b1cd5381d060e285af280b
Collin Squires <squirrelly392@gmail.com>
2023-11-07 19:26:45 -0700

    added DOM manipulation

--- lesson2/scripts/bom.js
+++ lesson2/scripts/bom.js
@@ -3,15 +3,22 @@ const button = document.querySelector("button");
 const list = document.querySelector("#list");
 
 button.addEventListener("click", () => {
-    console.log("clicked");
+  console.log("clicked");
 
-    if (input.value)
-    {
-        
-    }
-    else {
-        input.focus()
-        feedback.textContent = "Please enter a value"
-    }
+  if (input.value) {
+    const li = document.createElement("li");
+    const deleteButton = document.createElement("button");
+    li.textContent = input.value;
+    deleteButton.textContent = "❌";
+    deleteButton.addEventListener("click", function () {
+      list.removeChild(li);
+      input.focus();
+      input.value = '';
+    });
+    li.append(deleteButton);
+    list.append(li);
+  } else {
+    input.focus();
+    feedback.textContent = "Please enter a value";
+  }
 });
-
