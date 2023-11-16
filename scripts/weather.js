01814ba2248c2ad5aebdc419548ea9b36abc8270
Collin Squires <squirrelly392@gmail.com>
2023-11-16 09:23:33 -0700

    added weather to home page

---
+++ scripts/weather.js
@@ -0,0 +1,54 @@
+const currentTemp = document.querySelector("#temperature");
+const weatherIcon = document.querySelector("#icon-weather");
+const captionDesc = document.querySelector("#weather");
+
+let lastTimestamp = 0;
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?lat=43.69&lon=-112.01&appid=c2448f82676f1c51f8aad097f7b429bc&units=imperial";
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      localStorage.setItem("recentWeatherDataHome", JSON.stringify(data));
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].main;
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", data.weather[0].description);
+  captionDesc.textContent = `${desc}`;
+}
+
+function checkTime() {
+  if (localStorage.getItem("visitTimestampHome")) {
+    lastTimestamp = parseInt(localStorage.getItem("visitTimestampHome"));
+    currentTimestamp = Date.now();
+    timeDifference = currentTimestamp - lastTimestamp;
+
+    if (timeDifference > 3600000) {
+      lastTimestamp = currentTimestamp;
+      localStorage.setItem("visitTimestampHome", lastTimestamp);
+      apiFetch();
+    } else {
+      displayResults(JSON.parse(localStorage.getItem("recentWeatherDataHome")));
+    }
+  } else {
+    localStorage.setItem("visitTimestampHome", Date.now());
+    apiFetch();
+  }
+}
+
+checkTime();
