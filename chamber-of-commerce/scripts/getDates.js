a666cc75a03f00a6363e8414f6c24113e422da54
Collin Squires <squirrelly392@gmail.com>
2023-11-30 10:54:28 -0700

    very basic chamber home page

---
+++ chamber-of-commerce/scripts/getDates.js
@@ -0,0 +1,7 @@
+const today = new Date();
+
+// let yearElement = document.querySelector('#year');
+// yearElement.textContent = today.getFullYear().toString();
+
+let lastModifiedElement = document.querySelector('#lastModified');
+lastModified.textContent = document.lastModified;
