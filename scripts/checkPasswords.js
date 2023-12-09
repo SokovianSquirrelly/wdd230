26eeadab622cf68e0364eef69390a95b3dff257a
Collin Squires <squirrelly392@gmail.com>
2023-12-09 13:15:08 -0700

    fixed password checking on form

--- scripts/checkPasswords.js
+++ scripts/checkPasswords.js
@@ -8,10 +8,16 @@ function checkPasswords() {
     if (pass1.value != pass2.value) {
 		message.textContent = "❗Passwords DO NOT MATCH!";
 		message.style.visibility = "show";
+		pass1.style.backgroundColor = "#fff0f3";
+		pass1.value = "";
 		pass2.style.backgroundColor = "#fff0f3";
 		pass2.value = "";
+		pass1.focus();
+
 	} else {
 		message.style.display = "none";
+		pass1.style.backgroundColor = "#fff";
+		pass1.style.color = "#000";
 		pass2.style.backgroundColor = "#fff";
 		pass2.style.color = "#000";
     }
